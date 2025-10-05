21cf8d93c645297763fc09aff0fb1f5deccbd8b9
Nui Narongwet <narongwet.m@gmail.com>
2025-10-06 04:40:27 +0700

    Update chisel and dependencies

--- docker/scripts/install-chisel.sh
+++ docker/scripts/install-chisel.sh
@@ -5,22 +5,22 @@ set -ex
 target_platform=$1
 install_target=$2
 
-chisel_version=v1.0.0
+chisel_version=v1.2.0
 archive_sha256=""
 
 go_arch=""
 case $target_platform in
     linux/arm/v7 )
         go_arch=arm
-        archive_sha256=d70204fa47ec642d98d4b8b1403084e763111255cb845a22660d6948a5cd29fd
+        archive_sha256=9032d26bf1fab57d603447ea86167207ea9419309534c2883ef5d2fcdf8df766
         ;;
     linux/arm64 )
         go_arch=arm64
-        archive_sha256=c92aa273e63a06e8cbc1d1c398d69cad349af31c5c7e1a555b4d72c9ce0856c8
+        archive_sha256=c12a73550e0337f3719b74a2e4ed65afcac5dc8a8c324c105f4c829ecb44f615
         ;;
     linux/amd64 )
         go_arch=amd64
-        archive_sha256=8bd42198c8d9a5115693a0d42aa79acfd410431d758e98d4b74d39bb94e5743f
+        archive_sha256=4cdfd73c1ec5a5c9134453ba4422a000274f04e01435f73ea67da941210e4af5
         ;;
     * )
         >&2 echo "unsupported chisel arch: $target_platform"
